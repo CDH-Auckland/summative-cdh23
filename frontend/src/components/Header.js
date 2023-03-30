7fe391ddb6b290bc45b6ec2389e73e01886b9a5a
cerincabraham <cerin.abraham@gmail.com>
2023-03-31 01:20:15 +1300

    Done header functionally dynamc title and back navigation

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
+
 import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
 
-function Header() {
+function Header(props) {
+
+
+  const backHandler = () => {
+    props.backNavigation();
+  }
+
   return (
     <div className='header'>
-      <div className='header__left'>
+      <div className='header__left' onClick={backHandler}>
         <ArrowBackOutlinedIcon fontSize='large' />
       </div>
       <div className='header__right'>
-        <h1>Selling Items</h1>
+        <h1>{props.title}</h1>
       </div>
     </div>
   )
--- frontend/src/pages/Sellingitem.js
+++ frontend/src/pages/Sellingitem.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 import Header from "../components/Header"
 
 
+
 function Sellingitem() {
+    const navigate = useNavigate();
+
+    const backNavigation = () => {
+        navigate("/buyandsell");
+    }
+
+
     return (
         <div>
-            <Header />
+            <Header title={"Selling Items"} backNavigation={backNavigation} />
 
         </div>
     )
