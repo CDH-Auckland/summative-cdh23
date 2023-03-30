a460aecb093db079c010fe0d0c473eb5a951dd81
cerincabraham <cerin.abraham@gmail.com>
2023-03-30 21:28:32 +1300

    create buyandSell page and add routes for that in routesjs file

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 import LandingPage from "./pages/Landing";
+import BuySell from "./pages/BuySell";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={< LandingPage />} />
+            <Route path="/buyandsell" element={< BuySell />} />
         </Routes>
     )
 }
