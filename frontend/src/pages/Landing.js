87b23e4d33a778078fa6291d3755616f9a3b1d50
Dave Hwang <110789408+ddhwangnz@users.noreply.github.com>
2023-03-30 22:20:27 +1300

    First commit

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Landing() {
-    return (
-        <div className='wrapper'>
-            <div className='landingpage'>
-
-            </div>
+  return (
+    <div className="wrapper">
+      <div className="landingpage">
+        <div className="landingpageRight">
+          <h1>Hello World</h1>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
