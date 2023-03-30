39ef78feacaa8d25e57b663ebf508a3ee9ca8b4a
cerincabraham <cerin.abraham@gmail.com>
2023-03-31 00:07:46 +1300

    edit inf file, add header, header__left, header__right class in headerjs file

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function header() {
   return (
-    <div>header</div>
+    <div className='header'>
+      <div className='header__left'>
+
+      </div>
+      <div className='header__right'>
+
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Sellingitem.js
+++ frontend/src/pages/Sellingitem.js
@@ -1,8 +1,63 @@
 import React from 'react'
 
+import ShoppingBagOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import ViewHeadlineOutlinedIcon from "@mui/icons-material/ViewHeadlineOutlined";
+import FormatListBulletedOutlinedIcon from "@mui/icons-material/FormatListBulletedOutlined";
+import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+import CreditCardOutlinedIcon from "@mui/icons-material/CreditCardOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
+import AssignmentOutlinedIcon from "@mui/icons-material/AssignmentOutlined";
+import RadioButtonCheckedOutlinedIcon from "@mui/icons-material/RadioButtonCheckedOutlined";
+import RemoveCircleOutlineOutlinedIcon from "@mui/icons-material/RemoveCircleOutlineOutlined";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import UpdateOutlinedIcon from "@mui/icons-material/UpdateOutlined";
+import ForumOutlinedIcon from "@mui/icons-material/ForumOutlined";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined";
+import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import BallotOutlinedIcon from "@mui/icons-material/BallotOutlined";
+
+
+
 function Sellingitem() {
     return (
-        <div>Sellingitem</div>
+        <div>Sellingitem
+
+
+            <ShoppingBagOutlinedIcon />
+            <DeleteOutlineOutlinedIcon />
+            <ViewHeadlineOutlinedIcon />
+            <FormatListBulletedOutlinedIcon />
+            <AppsOutlinedIcon />
+            <CreditCardOutlinedIcon />
+            <ShoppingCartOutlinedIcon />
+            <NotificationsNoneOutlinedIcon />
+            <LocalShippingOutlinedIcon />
+            <AssignmentOutlinedIcon />
+            <RadioButtonCheckedOutlinedIcon />
+            <RemoveCircleOutlineOutlinedIcon />
+            <AddCircleOutlineOutlinedIcon />
+            <SendOutlinedIcon />
+            <PersonOutlineOutlinedIcon />
+            <UpdateOutlinedIcon />
+            <ForumOutlinedIcon />
+            <Inventory2OutlinedIcon />
+            <SearchOutlinedIcon />
+            <LogoutOutlinedIcon />
+            <ArrowDropDownOutlinedIcon />
+            <ArrowBackOutlinedIcon />
+            <HomeOutlinedIcon />
+            <BallotOutlinedIcon />
+        </div>
     )
 }
 
