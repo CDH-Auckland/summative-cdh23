1d6fba1f6cba77fa698e9658e2b98c40e4015770
Dave Hwang <110789408+ddhwangnz@users.noreply.github.com>
2023-03-31 11:04:05 +1300

    update mui material icons and landing page design

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -3,11 +3,11 @@ import DIOLogoR from "../images/DIO_LogoR.png";
 import background from "../images/BG_L_R.jpg";
 
 function Landing() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [firstName, setsignFirstName] = useState("");
+  const [lastName, setsignLastName] = useState("");
+  const [email, setsignEmail] = useState("");
+  const [password, setsignPassword] = useState("");
+  const [confirmPassword, setsignConfirmPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,74 +21,77 @@ function Landing() {
         <div className="landingpage__login"> </div>
         <div className="landingpage__signup">
           <div
-            className="landingpage__login_bg_r"
+            className="landingpage__signup__bgr"
             style={{
               backgroundImage: `url(${background})`,
-              height: "960px",
-              width: "1080px",
+              height: "1080px",
             }}
           >
-            <img className="DIOLogoR" src={DIOLogoR} alt="DIO Logo R" />
-            <h2>
-              Create an account by filling the form below to buy & sell your
-              kits
-            </h2>
+            <div className="landingpage__signup__contents">
+              <img
+                className="landingpage__signup__DIOLogoR"
+                src={DIOLogoR}
+                alt="DIO Logo R"
+              />
+              <h3>
+                Create an account by filling the form below to buy & sell your
+                kits
+              </h3>
 
-            <form className="landingpage__signup_input" onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="firstName"></label>
-                <input
-                  type="text"
-                  id="firstName"
-                  placeholder="First Name"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
+              <div
+                className="landingpage__signup__input"
+                onSubmit={handleSubmit}
+              >
+                <div>
+                  <input
+                    type="text"
+                    id="firstName"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={(e) => setsignFirstName(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    id="lastName"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={(e) => setsignLastName(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setsignEmail(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setsignPassword(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={(e) => setsignConfirmPassword(e.target.value)}
+                  />
+                </div>
+                <button className="landingpage__signup__button" type="submit">
+                  <h2>Sign Up</h2>
+                </button>
               </div>
-              <div>
-                <label htmlFor="lastName"></label>
-                <input
-                  type="text"
-                  id="lastName"
-                  placeholder="Last Name"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="email"></label>
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="password"></label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="confirmPassword"></label>
-                <input
-                  type="password"
-                  id="confirmPassword"
-                  placeholder="Confirm Password"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </div>
-              <button className="landingpage__signup_button" type="submit">
-                <h2>Sign Up</h2>
-              </button>
-            </form>
+            </div>
           </div>
         </div>
       </div>
