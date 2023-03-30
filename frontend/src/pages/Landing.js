5708d1986f4ec15e1f0db9a4581cd005923bee50
cerincabraham <cerin.abraham@gmail.com>
2023-03-30 22:41:19 +1300

    update landing page  with new css class names

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -4,6 +4,12 @@ function Landing() {
     return (
         <div className='wrapper'>
             <div className='landingpage'>
+                <div className='landingpage__login'>
+
+                </div>
+                <div className='landingpage__signup'>
+
+                </div>
 
             </div>
         </div>
