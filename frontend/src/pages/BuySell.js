7191278c1556eca8c44d2037fac3a1898d88f357
Dave Hwang <110789408+ddhwangnz@users.noreply.github.com>
2023-04-04 10:32:30 +1200

    done wishlist page

--- frontend/src/pages/BuySell.js
+++ frontend/src/pages/BuySell.js
@@ -1,12 +1,21 @@
-import React from 'react'
-
+import React from "react";
 
 function BuySell() {
-    return (
-        <div className='buysell'>
+  return (
+    <div className="BuySell">
+      <div className="buysell__buyitems">
+        <button className="buysell__buyitems__button" type="submit">
+          <h6>Browse items</h6>
+        </button>
+      </div>
 
-        </div>
-    )
+      <div className="buysell__sellitems">
+        <button className="buysell__sellitems__button" type="submit">
+          <h6>Selling items</h6>
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default BuySell
+export default BuySell;
