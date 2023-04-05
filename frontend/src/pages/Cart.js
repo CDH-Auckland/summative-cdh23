693899c1a8c9378aa2aa3d85b4a9563336704f52
Hannah Han <hannah13uk@gmail.com>
2023-04-05 22:30:12 +1200

    cart page design done

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Header from "../components/Header";
+import Statusmenu from "../components/Statusmenu"; // import Statusmenu component
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import CartBox from "../components/CartBox";
 
@@ -40,15 +41,31 @@ function Cart() {
   const backNavigation = () => {
     navigate("/browseitems");
   };
-
   const cartboxDelete = (e) => {
     console.log(`Deleting selected orderhistory ${e}`);
   };
+  const totalPrice = CartHistory.reduce(
+    (accumulator, currentValue) =>
+      accumulator + parseInt(currentValue.item_price.slice(4)),
+    0
+  );
+  const calculateTax = (totalPrice) => {
+    const taxRate = 0.13;
+    const taxAmount = totalPrice * taxRate;
+    return taxAmount.toFixed(2);
+  };
+  const taxAmount = calculateTax(totalPrice);
+
+  const sum = totalPrice + parseFloat(taxAmount);
 
   return (
     <div className="wrapper">
       <Header title={"Cart"} backNavigation={backNavigation} />
       <div className="wrapper__sub">
+        <Statusmenu
+          username="John Doe" // Pass props to Statusmenu component
+          cartCount={CartHistory.length} // Pass props to Statusmenu component
+        />
         <div className="cart_titleblock paddingtop__small">
           <h3>Product added to your cart </h3>
           <div className="cart__titleblock__icon">
@@ -64,7 +81,23 @@ function Cart() {
             />
           ))}
         </div>
-        {/* Create you div here */}
+        <div className="cart_total">
+          <h5 className="cart_total_h5-fontweight300"> Total</h5>
+          <h1 className="cart_total_h1-fontweight300">${totalPrice}</h1>
+        </div>
+
+        <div className="cart_taxes_line">
+          <h5 className="cart_taxes">Taxes & Fees</h5>
+          <h5 className="cart_taxes_sum">${taxAmount}</h5>
+        </div>
+
+        <div className="cart_subtotal_line">
+          <h5 className="cart_subtotal">SubTotal</h5>
+          <h3 className="cart_subtotal_sum">${sum}</h3>
+        </div>
+
+        <h5 className="cart_point">You earn +{totalPrice} points</h5>
+        <button className="cart_button">CHECKOUT</button>
       </div>
     </div>
   );
